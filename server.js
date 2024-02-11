2b57f36d26c4835c80e6737c8d79c6dba2f1152a
Ferzender Varli <fvarlii@gmail.com>
2024-02-11 23:07:43 +0300

    installed dotenv and added router

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
-const PORT = 5000;
+const users = [
+    { id: 1, name: 'John Doe', place: 'US' },
+    { id: 2, name: 'Jane Doe', place: 'UK' }
+]
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.json(users);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
